ba5397d515f5bc03059ae44ee968385f52be3eb5
RosVor <ros.vorokh@gmail.com>
2023-11-24 08:55:07 +0200

    overlay close

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import '../css/styles.css';
 
-const Modal = ({ largeImageURL, onClose }) => (
-  <div className="Overlay" onClick={onClose}>
-    <div className="Modal">
-      <img src={largeImageURL} alt="" />
+const Modal = ({ largeImageURL, onClose }) => {
+  const handleBackDropClick = (e) => {
+    if (e.target === e.currentTarget) {
+      onClose();
+    }
+  };
+
+  return (
+    <div className="Overlay" onClick={handleBackDropClick}>
+      <div className="Modal">
+        <img src={largeImageURL} alt="" />
+      </div>
     </div>
-  </div>
-);
+  );
+};
+
 
 export default Modal;
 
